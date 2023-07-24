5a25f17f4b9633109c080c2097974a27803ba98c
mmartinga <mercedes.martin@iteriam.es>
2023-07-24 19:56:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tabs-ionic-standalone',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
