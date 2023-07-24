271281718c61f23db600219006739c69dec75b1e
mmartinga <mercedes.martin@iteriam.es>
2023-07-24 20:19:14 +0200

    update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tabs-ionic-standalone',
+  appName: 'my-notes',
   webDir: 'www',
   server: {
     androidScheme: 'https'
